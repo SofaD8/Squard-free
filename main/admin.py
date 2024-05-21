c6b75dfa4deeb7da7158e3d0daaaa8779845a241
Sofa Davlietshyna <sofo4kavakula@gmail.com>
2024-05-21 15:28:55 +0300

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,56 @@
 from django.contrib import admin
 from .models import About, Services, PortfolioCategory, Photo, Team, Contact
+from django.utils.safestring import mark_safe
 
 
 # Register your models here.
-admin.site.register(About)
-admin.site.register(Services)
-admin.site.register(PortfolioCategory)
-admin.site.register(Photo)
-admin.site.register(Team)
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'sort')
+    list_editable = ('name', 'description','sort')
+    search_fields = ('name',)
+
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description','is_visible','sort')
+    list_editable = ('name', 'description', 'is_visible','sort')
+    list_filter = ('is_visible',)
+    search_fields = ('name',)
+
+
+@admin.register(PortfolioCategory)
+class PortfolioCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'sort')
+    list_editable = ('name', 'sort')
+    search_fields = ('name',)
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('photo_src_tag', 'sort')
+    list_editable = ('sort',)
+    list_filter = ('category',)
+    search_fields = ('sort',)
+
+    def photo_src_tag(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width=50 height=50>')
+
+    photo_src_tag.short_description = 'Photo'
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('photo_src_tag', 'id', 'name', 'description','sort')
+    list_editable = ('name', 'description', 'sort')
+    search_fields = ('name', 'description')
+
+    def photo_src_tag(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width=50 height=50>')
+
+    photo_src_tag.short_description = 'Team'
+
+
 admin.site.register(Contact)
