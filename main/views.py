d8a200e6c6cefad2d90339afea517edbcfd2c795
Sofa Davlietshyna <sofo4kavakula@gmail.com>
2024-05-22 17:43:18 +0300

    nav

--- main/views.py
+++ main/views.py
@@ -6,13 +6,17 @@ from .models import About, Services, PortfolioCategory, Photo, Team, Contact
 # Create your views here.
 def index(request):
     categories = PortfolioCategory.objects.all()
+    photo = Photo.objects.all()
 
     context = {
         'title_portfolio': 'Portfolio',
         'p_portfolio': 'Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.',
         'categories': categories,
+        'photos': photo,
         'title_team': 'Team',
         'p_team': 'Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.',
+        'team': Team.objects.all(),
+
     }
 
     return render(request, 'main.html', context=context)
