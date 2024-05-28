9ec349aa9c3e1b200649e9afa2657d1c48fec2ce
Sofa Davlietshyna <sofo4kavakula@gmail.com>
2024-05-28 23:19:56 +0300

    context_proc

--- main/context_processors.py
+++ main/context_processors.py
@@ -2,3 +2,27 @@ def app_title(request):
     return {
         'app_title': '<span>Squadfree</span>'
     }
+
+
+def app_address(request):
+    return {
+        'app_address': 'A108 Adam Street, New York, NY 535022'
+    }
+
+
+def app_phone(request):
+    return {
+        'app_phone': '+1 5589 55488 55'
+    }
+
+
+def app_contact(request):
+    return {
+        'app_email': 'contact@example.com'
+    }
+
+
+def app_info(request):
+    return {
+        'app_email': 'info@example.com'
+    }
